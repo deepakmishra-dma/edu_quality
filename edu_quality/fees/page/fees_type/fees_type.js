d6c0590cb785f8a088f04e8f4c20eb8644d2b2f0
Sankara Subramanian <sankara@walnutedu.in>
2023-06-17 14:31:06 +0000

    feat: Fee Type Table, Functions
    Fetch, update, delete table items

--- edu_quality/fees/page/fees_type/fees_type.js
+++ edu_quality/fees/page/fees_type/fees_type.js
@@ -51,6 +51,70 @@ async function fee_type_form(wrapper){
   </tbody></table> `
 	container.append(form)
 	get_fin_yr()
+	table = `
+	<div style='height:50px'></div>
+	<style>
+	.table3 th{
+		border: 1px solid black;
+		padding: 4px 5px;
+		border-collapse: collapse;
+	  }
+	.table3 th {
+		padding: 4px 5px;
+		text-align: left;
+		background-color: #F8B259;
+		color: #FFFFFF;
+		opacity: 0.8;
+	}
+	.table3 td{
+		padding: 4px 5px;
+	}
+	.table3, .table3 th, .table3 td {
+		border: 1px solid #cce6ff;
+		border-collapse: collapse;
+	}
+	</style>
+	<table cellpadding="0" cellspacing="0" id='table3' class="table3" style="width:100%; margin:0 auto;">
+	</table>`
+	container.append(table)
+	get_table()
+}
+
+
+function delete_item(item){
+	frappe.call({
+		method: 'delete_fee_type',
+		type: "POST",
+		args: {
+			'fee_type': item
+		},
+	callback: function(r){
+		row = document.getElementById("row-"+item).remove()
+	}
+})
+}
+
+async function get_table(){
+	frappe.db.get_list('Fee Type',{fields:['type','description','financial_year']}).then(
+		res =>{
+			var table = document.getElementById('table3')
+			table.innerHTML = ''
+			var html = `<tbody><tr>
+							<th>Head Type Name</th>
+							<th>Head Type Description</th>
+							<th>Financial Year</th>
+							<th style="">Del</th>
+						</tr>`
+			res.forEach(ft =>{
+				html = html + `<tr id="row-`+ ft.type + `"><td>`+ ft.type + "</td>"
+				html = html +  "<td>"+ ft.description + "</td>"
+				html = html +  "<td>"+ ft.financial_year + "</td>"
+				html = html + `<td><img id='`+ ft.type + `' width="18" height="18" src="/files/remove.png" title="Remove this" onclick=delete_item(this.id)></td></tr>`
+			})
+			html = html + '</tbody>'
+			table.innerHTML = html
+		}
+	)
 }
 
 function get_fin_yr(){
@@ -67,7 +131,7 @@ function get_fin_yr(){
 	)
 }
 
-function submit_form(){
+async function submit_form(){
 	var fin_yr = document.getElementById('head_type_academic_year').value 
 	if(fin_yr == 'Select Year'){
 		frappe.throw('Please Select Financial Year!')
@@ -89,7 +153,9 @@ function submit_form(){
 			'fin_yr': fin_yr,
 			'desc': description,
 			'fee_type': fee_type
-		}
-	})
-	
+		},
+	callback: function(r){
+		get_table()
+	}
+})
 }
