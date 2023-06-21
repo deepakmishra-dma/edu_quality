d72e541881456aa072ee72a5fd4242109b79d795
vikaswalnutedu <136430185+vikaswalnutedu@users.noreply.github.com>
2023-06-21 11:24:06 +0000

    Update tasks.py

--- edu_quality/tasks.py
+++ edu_quality/tasks.py
@@ -3,4 +3,4 @@ import frappe
 
 def cron():
     frappe.msgprint("Scheduler for MySQL is running")
-    frappe.enqueue("edu_quality.mysql.migrate_mysql", queue="long", timeout=1500)
+    frappe.enqueue("edu_quality.mysql.migrate_mysql", is_async=True)
