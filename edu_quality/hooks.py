c9f6ccf3a68bc7f1d5d9cb1357fcb1cdf0a00825
Sankara Subramanian <sankara@walnutedu.in>
2023-06-23 15:40:07 +0000

    feat: generate reference, payment plan

--- edu_quality/hooks.py
+++ edu_quality/hooks.py
@@ -29,7 +29,10 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Student Applicant" : "public/js/application.js"}
+doctype_js = {
+    "Student Applicant" : "public/js/application.js",
+    "Reference Number Settings": "public/js/reference_number.js"
+              }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
