187187ccfb2a06d37d3c84b10dd008990c755a52
Badal <badalsahani8381@gmail.com>
2023-06-26 12:02:51 +0000

    fix: override get_payment_url in payment request

--- edu_quality/overrides.py
+++ edu_quality/overrides.py
@@ -8,7 +8,8 @@ from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
 from erpnext.accounts.doctype.payment_entry.payment_entry import (
     get_company_defaults,
 )
-from erpnext.accounts.doctype.payment_request.payment_request import PaymentRequest
+from erpnext.accounts.doctype.payment_request.payment_request import PaymentRequest, _get_payment_gateway_controller
+from frappe.utils.data import flt
 
 
 class CustomPaymentRequest(PaymentRequest):
@@ -64,6 +65,38 @@ class CustomPaymentRequest(PaymentRequest):
             )
 
         frappe.db.set_value(fees.doctype, fees.name, "outstanding_amount", 0)
+
+    def get_payment_url(self, **kwargs):
+        if self.reference_doctype != "Fees":
+            data = frappe.db.get_value(
+				self.reference_doctype, self.reference_name, ["company", "customer_name"], as_dict=1
+			)
+        else:
+            data = frappe.db.get_value(
+				self.reference_doctype, self.reference_name, ["student_name"], as_dict=1
+			)
+            data.update({"company": frappe.defaults.get_defaults().company})
+        controller = _get_payment_gateway_controller(self.payment_gateway)
+        
+        controller.validate_transaction_currency(self.currency)
+        
+        if hasattr(controller, "validate_minimum_transaction_amount"):
+            controller.validate_minimum_transaction_amount(self.currency, self.grand_total)
+            
+        return controller.get_payment_url(
+			**{
+				"amount": flt(self.grand_total, self.precision("grand_total")),
+				"title": data.company.encode("utf-8"),
+				"description": self.subject.encode("utf-8"),
+				"reference_doctype": "Payment Request",
+				"reference_docname": self.name,
+				"payer_email": self.email_to or frappe.session.user,
+				"payer_name": frappe.safe_encode(data.customer_name),
+				"order_id": self.name,
+				"currency": self.currency,
+                "payment_method": kwargs.get("payment_method")
+			}
+		)
 
 
 def payment_entry(doc, ref_doc, party_amount, paid_from, paid_to, company, cost_center):
