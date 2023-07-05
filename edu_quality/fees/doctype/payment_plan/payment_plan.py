0fba8f350c5a43eee18f3f845f273edaca169bc2
vikaswalnutedu <vikas@walnutedu.in>
2023-07-05 12:57:46 +0000

    fix: payment plan

--- edu_quality/fees/doctype/payment_plan/payment_plan.py
+++ edu_quality/fees/doctype/payment_plan/payment_plan.py
@@ -1,15 +1,23 @@
 # Copyright (c) 2023, Hybrowlabs Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class PaymentPlan(Document):
-	def before_save(self):
+	def before_validate(self):
+		self.payment_schedule = []
+		invoice_portion = 0
 		for schedule in self.payment_schedule:
 			if schedule.invoice_portion:
-				schedule.payment_amount = self.total_amount * (schedule.invoice_portion/100)
-			elif schedule.payment_amount:
-				schedule.invoice_portion = (schedule.payment_amount/self.total_amount) *100
+				invoice_portion += schedule.invoice_portion
+		if invoice_portion != 100:
+			frappe.throw("Invoice Portion should be equal to 100")
+		else:
+			for schedule in self.payment_schedule:
+				if schedule.invoice_portion:
+					schedule.payment_amount = self.total_amount * (schedule.invoice_portion/100)
+				elif schedule.payment_amount:
+					schedule.invoice_portion = (schedule.payment_amount/self.total_amount) *100
 
 
