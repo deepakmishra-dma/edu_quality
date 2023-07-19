fe5dd04adee6c26c29f023ac661e1768b4074d53
Sankara Subramanian <sankara@walnutedu.in>
2023-07-19 17:04:39 +0000

    fix: edu quality

--- edu_quality/public/py/student.py
+++ edu_quality/public/py/student.py
@@ -9,9 +9,9 @@ def autoname(doc,method=None):
             prefix = frappe.get_value("Prefix Table",{'school':applicant.school},'prefix')
         if frappe.db.exists("Reference Number Table",{'program':applicant.program}):
             series = frappe.get_value("Reference Number Table",{'program':applicant.program},'series')
-            prefix += "-" + series + "-"
+            prefix += series
         if frappe.db.count("Student",[["name","Like","%prefix%"]])>=99:
-            prefix = prefix[:-2] + chr(ord(prefix[-2]) + 1) + "-"
+            prefix = prefix[:-2] + chr(ord(prefix[-2]) + 1)
             series = series[0] + chr(ord(series[1])+1)
             frappe.db.set_value("Reference Number Table",{'program':applicant.program},'series',series)
         if not prefix:
