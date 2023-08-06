c1ffe943a6b6c4726908fe88772b346845242d9d
vikaswalnutedu <vikas@walnutedu.in>
2023-08-06 13:55:02 +0000

    feat: add cron to create payment request before due_date

--- edu_quality/tasks.py
+++ edu_quality/tasks.py
@@ -1,4 +1,5 @@
 import frappe
+from datetime import datetime
 
 
 def cron():
@@ -17,8 +18,34 @@ def cron():
 
 def time_based():
     frappe.enqueue(
-            "edu_quality.discount.time_based_discount",
-            is_async=True,
-            queue="long",
-            timeout=1800
-        )
+        "edu_quality.discount.time_based_discount",
+        is_async=True,
+        queue="long",
+        timeout=1800,
+    )
+
+
+def create_payment_request_before_due_date():
+    before_days = frappe.db.get_single_value("Fees Settings", "before_days")
+    today = frappe.utils.nowdate()
+    today = datetime.date(datetime.strptime(today, "%Y-%m-%d"))
+    fee_schedule = frappe.get_all("Fee Schedule")
+    for f in fee_schedule:
+        fs = frappe.get_doc("Fee Schedule", f.name)
+        if fs.due_date:
+            if (fs.due_date - today).days < before_days:
+                fees = frappe.get_all(
+                    "Fees",
+                    {
+                        "fee_structure": fs.fee_structure,
+                        "docstatus": 1,
+                        "outstanding_amount": [">", 0],
+                    },
+                )
+                frappe.enqueue(
+                    "edu_quality.public.py.student.create_payment_request",
+                    fees=fees,
+                    is_async=True,
+                    queue="long",
+                    timeout=1800,
+                )
