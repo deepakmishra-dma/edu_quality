ed357d0560f995b33637d0b6efc1d75a18fda83b
vikaswalnutedu <vikas@walnutedu.in>
2023-08-13 12:20:40 +0000

    fix: payment plan creation tool change

--- edu_quality/fees/doctype/payment_plan/payment_plan.py
+++ edu_quality/fees/doctype/payment_plan/payment_plan.py
@@ -19,4 +19,23 @@ class PaymentPlan(Document):
 				elif schedule.payment_amount:
 					schedule.invoice_portion = (schedule.payment_amount/self.total_amount) *100
 
+		existing_payment_plan = frappe.db.get_all("Payment Plan", {
+                "academic_year": self.academic_year,
+                "school": self.school,
+                "program": self.program
+            }, ["name"])
+
+		if existing_payment_plan:
+			for plan in existing_payment_plan:
+				existing_payment_plan_doc = frappe.get_doc("Payment Plan", plan.name)
+				existing_schedule = existing_payment_plan_doc.payment_schedule
+				if len(existing_schedule) == len(self.payment_schedule):
+					# Compare payment terms, due dates, and invoice portions
+					is_identical = all(
+						term.payment_term == existing_term.payment_term and
+						term.invoice_portion == existing_term.invoice_portion
+						for term, existing_term in zip(self.payment_schedule, existing_schedule)
+					)
+					if is_identical:
+						frappe.throw("Identical Payment Plan already exists")
 
