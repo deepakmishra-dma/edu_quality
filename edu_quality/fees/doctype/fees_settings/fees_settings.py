ac955e7c88019e2e7d4dbbe3d66dd8ca5f92e0d2
Sankara Subramanian <sankara@walnutedu.in>
2023-08-19 16:45:56 +0000

    fix: task 126

--- edu_quality/fees/doctype/fees_settings/fees_settings.py
+++ edu_quality/fees/doctype/fees_settings/fees_settings.py
@@ -1,8 +1,22 @@
 # Copyright (c) 2023, Hybrowlabs Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class FeesSettings(Document):
-	pass
+	def validate(self):
+		#validate duplicates in Application Fees
+		i=0
+		j=0
+		for ap_fee in self.application_fees:
+			j=0
+			for next in self.application_fees:
+				if i!=j and ap_fee.class_name == next.class_name and ap_fee.academic_year == next.academic_year:
+					frappe.throw("Application fee for " + next.class_name + "-" + next.academic_year + " Already Exists!")
+				j+=1
+			i+=1
+
+
+
+
