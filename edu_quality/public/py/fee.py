7139fdb1e52b82a10b9388f1159f1027809716c0
Sankara Subramanian <sankara@walnutedu.in>
2023-10-01 05:10:50 +0000

    fix: fees

--- edu_quality/public/py/fee.py
+++ edu_quality/public/py/fee.py
@@ -19,14 +19,14 @@ def after_insert(doc,method=None):
 
 def before_submit(doc,method=None):
     time_based_discount(doc)
-    update_payment_schedule(doc)
     referal_discount(doc)
+    update_payment_schedule(doc)
 
 
 def before_update(doc,method=None):    
     old_doc = doc.get_doc_before_save()
     if old_doc.payment_schedule != doc.payment_schedule:
-        for term,old_term in zip(doc.payment_schedule,old_doc.payment_term):
+        for term,old_term in zip(doc.payment_schedule,old_doc.payment_schedule):
             if old_term.outstanding == 0 and term.outstanding != 0:
                 frappe.throw("Cannot Change term - " + term.payment_term + " As it is already Paid!")
             if term.invoice_portion:
