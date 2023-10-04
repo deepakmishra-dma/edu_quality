8603a77205f0359b7d6195ac86637a4ed57ecc1c
aguyran <aguyran@gmail.com>
2023-10-04 17:26:47 +0000

    add: master for school

--- edu_quality/hooks.py
+++ edu_quality/hooks.py
@@ -274,7 +274,7 @@ fixtures = [
     {
         "dt": "Custom DocPerm",
         "filters": [["module","=","Edu Quality"]]
-    }, {"dt":"Program"},    {"dt":"Lead Sub Status"},
+    }, {"dt":"Program"},    {"dt":"Lead Sub Status"},{"dt":"School"},{"dt":"Academic Year"},
     {"dt":"Email Template"},
     {"dt":"Letter Head"},
     {'dt':"Workspace",
