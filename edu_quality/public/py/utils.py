ee464b29c38f4b64c5efd1a56c55b4a94101fed3
vikaswalnutedu <vikas@walnutedu.in>
2023-10-19 20:43:19 +0000

    fix: undertaking OTP

--- edu_quality/public/py/utils.py
+++ edu_quality/public/py/utils.py
@@ -127,24 +127,8 @@ def generate_otp(fee):
     except Exception as e:
         return False
 
-@frappe.whitelist()
-def generate_undertaking_otp(payment_hash):
-    fee = get_fee(payment_hash)
-    return generate_otp(fee)
 
-
-@frappe.whitelist()
-def verify_undertaking_otp(payment_hash, otp):
-    fee = get_fee(payment_hash)
-    return verify_otp(fee, otp)
-
-
-def get_fee(payment_hash):
-    fee = frappe.get_value("Payment Request", {"payment_hash": payment_hash}, "reference_name")
-    return fee
-
-
-def send_otp(fee, otp):
+def send_otp(fee,otp):
     try:
         student = frappe.get_value("Fees",fee,"student")
         student = frappe.get_doc("Student",student)
@@ -154,18 +138,16 @@ def send_otp(fee, otp):
             email = student.custom_mothers_email
         elif student.custom_guardians_email_id:
             email = student.custom_guardians_email_id
-        elif student.student_email_id:
-            email = student.student_email_id
         if email:
-            subject = "OTP for Undertaking Submission"
-            message = f"OTP for Undertaking Submission is {otp}"
-            email_otp(email, subject, message)
+            email_otp(email,otp)
         #whatsapp message
         return True
     except Exception as e:
         return False
     
-def email_otp(email, subject=None, message=None):
+def email_otp(email,otp):
+    subject = "OTP for Changing Payment Plan"
+    message = f"OTP for Changing Payment Plan is {otp}"
     frappe.sendmail(
         recipients=email, subject=subject, message=message, delayed=False
     )
