3a2c4b79c2cfc6a7cfa24d4e3223c4104d3104f8
Sankara Subramanian <sankara@walnutedu.in>
2023-11-13 08:17:06 +0000

    fix: add logger

--- edu_quality/public/py/discount.py
+++ edu_quality/public/py/discount.py
@@ -214,6 +214,7 @@ def payment_plan(doc, method=None):
             if fee_type and fee_type!= "Regular":
                 initial_payment = initial_payment +  component.amount
         i=0
+        frappe.logger('initial').exception("initial_payment"+str(initial_payment))
         for schedule in pp.payment_schedule:
             payment_amount = schedule.payment_amount
             description = "Installment - " + str(i+1)
@@ -222,7 +223,7 @@ def payment_plan(doc, method=None):
                 today = datetime.today().date()
                 difference = schedule.due_date - today
                 if difference.days > before_days:
-                        frappe.logger("pr").exception("only deposit")
+                        frappe.logger('initial').exception("only deposit")
                         only_deposit(doc)
                 else:
                     payment_amount = payment_amount + initial_payment
