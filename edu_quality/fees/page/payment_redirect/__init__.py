f7feef4818f0d749d2896fb7c0c07a6c4f327c6d
Sankara Subramanian <sankara@walnutedu.in>
2023-11-15 07:32:14 +0000

    fix: payment page

--- edu_quality/fees/page/payment_redirect/__init__.py
+++ edu_quality/fees/page/payment_redirect/__init__.py
@@ -49,6 +49,7 @@ def get_payment_details(**kwargs):
                     })
                     is_deposit = True
             else:
+            
                 breakup.append({
                     'fees_category': fee.fees_category,
                     'amount':  frappe.utils.fmt_money(amount *(portion/100), currency="INR"),
@@ -71,12 +72,6 @@ def get_payment_details(**kwargs):
                     'company': company
                 })
                 is_deposit = True
-            else:
-                breakup.append({
-                    'fees_category': fee.fees_category,
-                    'amount':  frappe.utils.fmt_money(amount *(portion/100), currency="INR"),
-                    'company': company
-                })
 
     return {
         'student_name': fees.student_name,
