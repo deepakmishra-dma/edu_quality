30fe67ab5c8afee7452379bf2e7d183d713b27d3
Sankara Subramanian <sankara@walnutedu.in>
2023-11-27 08:44:36 +0000

    fix: manual payment

--- edu_quality/public/py/manual_payment.py
+++ edu_quality/public/py/manual_payment.py
@@ -1,5 +1,5 @@
 import frappe
-
+from frappe.utils.data import flt
 
 @frappe.whitelist()
 def manual_payment(fee,term,data):
@@ -33,3 +33,60 @@ def update_reference(reference_no, entry):
     frappe.db.set_value("Payment Entry", entry.name, "reference_no", reference_no)
     frappe.db.set_value("Payment Entry", entry.name, "reference_date", date)
     frappe.db.set_value("Payment Entry", entry.name, "remarks", remarks)
+
+
+@frappe.whitelist()
+def get_payment_details(fee,term):
+    try:
+        fee = frappe.get_doc("Fees",fee)
+        data = []
+        portion = 100
+        description = ""
+        if term != 'Depsoit':
+            for schedule in fee.payment_schedule:
+                if schedule.payment_term == term:
+                    portion  = schedule.invoice_portion
+                    description = schedule.description
+                    break 
+            
+        for component in fee.components:
+            fee_type = frappe.db.get_value("Fee Category",component.fees_category,"type")
+            amount = component.custom_amount_after_discount or component.amount
+            company = frappe.db.get_value("Fee Category",component.fees_category,"custom_company")
+            if not company:
+                company = "Unique Educational and Sports Foundation"
+            if fee_type !='Regular' and term == 'Deposit':
+                entry ={
+                    "company": company,
+                    "amount": flt(amount,2),
+                    "reference":""
+                }
+            elif term != 'Deposit':
+                if fee_type!='Regular' and 'deposit' in description:
+                    entry ={
+                    "company": company,
+                    "amount": flt(amount,2),
+                    "reference":""
+                    }
+                elif fee_type == "Regular":
+                    entry = {
+                    "company": company,
+                    "amount": flt(amount *(portion/100),2),
+                    "reference":""
+                    }
+            data = company_wise(data,entry)
+        return data
+    except Exception as e:
+        frappe.logger('manual').exception(e)
+
+
+def company_wise(data, component):
+    f=0
+    for i in data:
+        if i.get('company') == component.get('company'):
+            i['amount'] += component.get('amount')
+            f=1
+            break
+    if f==0:
+        data.append(component)
+    return data
