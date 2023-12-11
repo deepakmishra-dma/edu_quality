02584b996d307872a7ed13d593d3ecef73fbc717
Sankara Subramanian <sankara@walnutedu.in>
2023-12-11 13:42:46 +0000

    feat: company wise entries

--- edu_quality/hooks.py
+++ edu_quality/hooks.py
@@ -109,6 +109,7 @@ override_doctype_class = {
     "Payment Request": "edu_quality.overrides.CustomPaymentRequest",
     "Fee Schedule": "edu_quality.public.py.fee_schedule.CustomFeeSchedule",
     "Program Enrollment": "edu_quality.public.py.enrollment_override.CustomProgramEnrollment",
+    "Fees": "edu_quality.edu_quality.overrides.fees.CustomFees"
 }
 
 # Document Events
