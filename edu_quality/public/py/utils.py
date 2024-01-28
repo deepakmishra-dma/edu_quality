6cc92a449c9138528959ae7f0c57be5ca74d47c2
Vikas Moin <vikas@walnutedu.in>
2024-01-28 14:00:41 +0000

    fix: quick fix

--- edu_quality/public/py/utils.py
+++ edu_quality/public/py/utils.py
@@ -327,7 +327,7 @@ def handle_undertaking_submission(**kwargs):
 
     if not frappe.db.exists(
         "Rules and Regulation Submission",
-        {"reference_no": student_doc.custom_reference_number},
+        {"student": student_doc.name},
     ):
         new_doc = frappe.new_doc("Rules and Regulation Submission")
         new_doc.student = student_doc.name
