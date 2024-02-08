be4f69d8b72ab986e2ffc49b8b6a2f81a7056709
vikaswalnutedu <136430185+vikaswalnutedu@users.noreply.github.com>
2024-02-08 14:21:23 +0000

    fix: quick fix

--- edu_quality/fees/doctype/fee_advance/fee_advance.py
+++ edu_quality/fees/doctype/fee_advance/fee_advance.py
@@ -288,6 +288,7 @@ def create_fee_advance(student, program_enrollment,all_len=None,index=None):
     try:
         if not frappe.get_value("Program", program_enrollment.program, "custom_is_passing_out_class"):
             frappe.log_error(title="Fee Advance",message="Since there is not a class scheduled for {class_name}, we will not be creating a fee advance.".format(class_name =program_enrollment.program))
+            return 
         if all_len and index:
             set_progress(index + 1, all_len,index, "Student Fees Details")
         school = frappe.get_value("Program", program_enrollment.program,"school")
