28f0154ee2b03744e06cd0c087f30bafa2f16d63
Sankara Subramanian <sankara@walnutedu.in>
2024-03-04 08:40:05 +0000

    feat: projected strength api

--- edu_quality/edu_quality/server_scripts/utils.py
+++ edu_quality/edu_quality/server_scripts/utils.py
@@ -3,16 +3,29 @@ from frappe.utils import today
 
 
 def is_rolled_over():
-    filter = [["Academic Year","year_start_date","<=",today()],
-			      ["Academic Year","year_end_date",">=",today()]]
-    if frappe.db.exists("Academic Year",filter):
-        return frappe.db.get_value("Academic Year",filter,"rolled_over")
+    current_yr = current_academic_year()
+    if current_yr:
+        return frappe.db.get_value("Academic Year",current_yr,"rolled_over")
     
 
 def previous_academic_year():
     filter = [["Academic Year","year_end_date","<=",today()]]
     if frappe.db.exists("Academic Year",filter):
         return frappe.db.get_value("Academic Year",filter,order_by="year_start_date")
+
+def current_academic_year():
+    filter = [["Academic Year","year_start_date","<=",today()],
+			      ["Academic Year","year_end_date",">=",today()]]
+    if frappe.db.exists("Academic Year",filter):
+        return frappe.db.get_value("Academic Year",filter,order_by="year_start_date")
+    
+def next_academic_year(current=None):
+    if not current:
+        current = current_academic_year()
+    current_end_date = frappe.db.get_value("Academic Year",current,"year_end_date")
+    filters = [["Academic Year","year_start_date",">",current_end_date]]
+    if frappe.db.exists("Academic Year",filters):
+        return frappe.db.get_value("Academic Year",filters,order_by="year_start_date")
     
 
 def get_previous_class(program):
@@ -42,3 +55,19 @@ def class_count_before_rollover(program_enrollment):
         ["Program Enrollment", "program","=",program_enrollment.program],
         ["Program Enrollment", "student_group","=",division],
     ]
+
+def get_next_class(current_class):
+    school,current_sequence = frappe.db.get_value("Program",current_class,["school","sequence"])
+    if frappe.db.exists("Program",{"school":school,"sequence":current_sequence+1}):
+        return frappe.db.get_value("Program",{"school":school,"sequence":current_sequence+1})
+    return None
+
+
+@frappe.whitelist()
+def projected_strength(current_class):
+    next_class = get_next_class(current_class)
+    strength = frappe.db.count("Program Enrollment",{"program":current_class,'academic_year':current_academic_year()})
+    if next_class:
+        strength += frappe.db.count("Program Enrollment",{"program":next_class,'academic_year':next_academic_year()})
+    return strength
+
