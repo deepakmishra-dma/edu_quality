3045b7a596b5c6d1dd5d768c6ebfd1a024804438
Sankara Subramanian <sankara@walnutedu.in>
2024-05-02 13:53:29 +0000

    fix: label

--- edu_quality/public/py/discount.py
+++ edu_quality/public/py/discount.py
@@ -640,6 +640,7 @@ def update_breakup_after_pp_change(fees):
                     new_breakup = update_discount_breakup(schedule.payment_amount, schedule.discount_breakup,
                                                                 discount,discount_amount,dis,0)
                     frappe.db.set_value("Payment Schedule",schedule.name,{'discount_breakup':new_breakup})
+                    
                     fees.reload()
                 elif term == 'All':
                     discount_amount = flt(breakup[dis]['discount_amount'] * schedule.invoice_portion/100,2)
