5e32c50c6e4b519ad5a155af03e5f51e6b71fde7
Vikas Moin <vikas@walnutedu.in>
2024-05-06 06:44:51 +0000

    fix: responce updated

--- edu_quality/common/mgr_enroll/enroll.py
+++ edu_quality/common/mgr_enroll/enroll.py
@@ -3,6 +3,6 @@ import frappe
 @frappe.whitelist(allow_guest=True)
 def make_enrollment_erp(token=None,lms_id=None,refno=None):
     if token!="***REMOVED-TOKEN***":
-        return {"status":"Error","msg":"Please pass correct token"}
+        return {"status":"Error","msg":"Please provide the correct token."}
     else:
-        pass
+        return {"status":"Success","msg":"Success"}
