5ace6d07d373c130308241c44bcd04c6c00fdd5c
aguyran <aguyran@gmail.com>
2024-05-09 09:36:02 +0000

    fix: creation of folder

--- edu_quality/edu_quality/doctype/carnival_event/carnival_event.py
+++ edu_quality/edu_quality/doctype/carnival_event/carnival_event.py
@@ -12,8 +12,10 @@ class CarnivalEvent(Document):
 
 @frappe.whitelist()
 def move_existing_and_upload_to_drive(**data):
+    folder_name = data.get("storedParams").get("folder_name")
+    # existing_folder = frappe.db.exists("File", {"name": f"Home/{folder_name}"})
     file_doc = frappe.get_doc("File", data.get("name"))
-    upload_file(file_doc.file_url, data.get("name"))
-    file_doc.folder = f"Home/{data.get('storedParams').get('folder_name')}"
+    upload_file(file_doc.file_url, folder_name)
+    file_doc.folder = f"Home/{folder_name}"
     file_doc.save()
     return "SUCCESS"
