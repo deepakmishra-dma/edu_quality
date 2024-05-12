d9913e221ee644f048a3ee378bd91e406ae2b4ee
Vikas Moin <vikas@walnutedu.in>
2024-05-12 15:15:49 +0000

    fix: mgr api

--- edu_quality/api/enroll_erp.py
+++ edu_quality/api/enroll_erp.py
@@ -1,5 +1,4 @@
 import frappe
-from edu_quality.public.py.application import enroll_student
 
 
 @frappe.whitelist(allow_guest=True)
@@ -7,22 +6,9 @@ def make_enrollment_erp(token=None, lms_id=None, refno=None):
     try:
         if token != "***REMOVED-TOKEN***":
             return {"status": "Error", "msg": "Please provide the correct token."}
-
-        student_applicant = frappe.get_value("Student Applicant", {"lms_id": lms_id})
-        if student_applicant:
-            application_status = frappe.get_value(
-                "Student Applicant", student_applicant, "application_status"
-            )
-            if application_status == "Admitted":
-                create_mgr_log(lms_id, refno, "Failed", "Student already enrolled")
-                return {"status": "Error", "msg": "Student already enrolled"}
-
-            enroll_student(student_applicant, refno)
-            create_mgr_log(lms_id, refno, "Success")
-            return {"status": "Success", "msg": "Successfully Enrolled"}
         else:
-            create_mgr_log(lms_id, refno, "Failed", "Student Applicant not found")
-            return {"status": "Error", "msg": "Student Applicant not found"}
+            create_mgr_log(lms_id, refno, "Pending")
+            return {"status": "Success", "msg": "Successfully Enrolled"}
 
     except Exception as e:
         create_mgr_log(lms_id, refno, "Failed", frappe.get_traceback())
