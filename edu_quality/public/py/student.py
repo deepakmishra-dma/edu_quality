ae5a7b90c92750ed4a2f823f58bf33c4918f3e55
Vikas Moin <vikas@walnutedu.in>
2024-05-18 09:45:08 +0000

    feat: fee details on student table

--- edu_quality/public/py/student.py
+++ edu_quality/public/py/student.py
@@ -103,3 +103,11 @@ def create_payment_request(fee,term=None):
             )
     except Exception as e:
         frappe.logger("edu_quality").exception(e)
+
+@frappe.whitelist()
+def get_fees_details(student):
+    class_id = frappe.get_value("Program Enrollment",{"student":student,"docstatus":1},"program",order_by = "creation desc")
+    if class_id and frappe.get_value("Fees",{"student":student,"program":class_id}):
+        return frappe.get_doc("Fees",{"student":student,"program":class_id}).payment_schedule
+    else:
+        return False
