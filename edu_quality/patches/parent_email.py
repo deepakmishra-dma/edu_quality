8deeb66c1fd3bc9abd306ad464dc3e47c4f330bc
Sankara Subramanian <sankara@walnutedu.in>
2024-07-08 04:22:56 +0000

    feat: replace parent yopmail UAT

---
+++ edu_quality/patches/parent_email.py
@@ -0,0 +1,10 @@
+import frappe
+
+def execute():
+    replace_emails()
+
+def replace_emails():
+    data = frappe.db.get_all("Guardian", fields=["name","email_id"])
+    for guardian in data:
+        new_email = guardian.email_id.split("@")[0] + "@yopmail.com"
+        frappe.db.set_value("Guardian",guardian.name,"email_id",new_email)
