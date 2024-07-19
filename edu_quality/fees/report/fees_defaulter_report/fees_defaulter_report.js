12f5c29a6ecf1c031ffe23107a65e3231e96ac42
vikaswalnutedu <vikas@walnutedu.in>
2024-07-19 11:56:41 +0000

    feat: create fee defaulter report

---
+++ edu_quality/fees/report/fees_defaulter_report/fees_defaulter_report.js
@@ -0,0 +1,29 @@
+// Copyright (c) 2024, Hybrowlabs Technologies and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Fees Defaulter Report"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+		},
+		{
+			"fieldname": "school",
+			"label": __("School"),
+			"fieldtype": "MultiSelectList",
+			"options": "School",
+			"width": "80",
+			get_data: function(txt) {
+				return frappe.db.get_link_options('School', txt);
+			}
+		}
+	]
+};
