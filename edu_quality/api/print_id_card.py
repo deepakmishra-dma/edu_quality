b3e111fd20014a7c6f3426fcf328680970891c0e
Vikas Moin <vikas@walnutedu.in>
2024-09-22 09:53:23 +0000

    feat: permanant id card changes

--- edu_quality/api/print_id_card.py
+++ edu_quality/api/print_id_card.py
@@ -37,3 +37,29 @@ def generate(**kwargs):
     )
     frappe.local.response.filecontent = main_pdf
     frappe.local.response.type = "pdf"
+
+
+@frappe.whitelist()
+def generate_permanent_id_cards(**kwargs):
+    base_url = frappe.utils.get_url()
+
+    program_enrollment = [
+        frappe.get_doc("Program Enrollment", enrollment)
+        for enrollment in kwargs.get("enrollments")
+    ]
+
+    enrollment_in_chunks = divide_into_subarrays(program_enrollment, 4)
+
+    template = frappe.render_template(
+        "edu_quality/templates/pdf/multiple_permanent_id_card.html",
+        {"program_enrollments": enrollment_in_chunks},
+    )
+    html = HTML(string=template, base_url=base_url)
+    main_doc = html.render()
+    main_pdf = main_doc.write_pdf()
+
+    frappe.local.response.filename = "Permanent Id Card.pdf".format(
+        name="Permanent Id Card.pdf".replace(" ", "-").replace("/", "-")
+    )
+    frappe.local.response.filecontent = main_pdf
+    frappe.local.response.type = "pdf"
