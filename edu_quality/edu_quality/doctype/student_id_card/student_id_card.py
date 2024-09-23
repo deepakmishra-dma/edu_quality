d63160c7e305f175f0e83766e5730b31987df5b1
vikaswalnutedu <vikas@walnutedu.in>
2024-09-23 13:21:54 +0000

    fix: student id card naming

--- edu_quality/edu_quality/doctype/student_id_card/student_id_card.py
+++ edu_quality/edu_quality/doctype/student_id_card/student_id_card.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2023, Hybrowlabs Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+
 
 class StudentIDCard(Document):
-	pass
-	
+    def autoname(self):
+        student = frappe.get_value(
+            "Program Enrollment", self.program_enrolled_in, "student"
+        )
+        academic_year = frappe.get_value(
+            "Program Enrollment", self.program_enrolled_in, "academic_year"
+        )
+        name = make_autoname(student + "-{" + academic_year + "}-.###")
+        self.name = name.replace("{", "(").replace("}", ")")
