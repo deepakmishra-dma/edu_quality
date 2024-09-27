9ed7cc8b3a7264a7777619123ab7bf6e0ea80d01
Sankara Subramanian <sankara@walnutedu.in>
2024-09-27 09:00:10 +0000

    fix: test patch

--- edu_quality/patches/guardian_user.py
+++ edu_quality/patches/guardian_user.py
@@ -3,7 +3,7 @@ from edu_quality.edu_quality.server_scripts.guardian import create_user, set_stu
 
 
 def execute():
-    guardians = frappe.get_all("Guardian")
+    guardians = frappe.get_all("Guardian",limit=100)
     for guardian in guardians:
         doc = frappe.get_doc("Guardian", guardian.name)
         create_user(doc)
