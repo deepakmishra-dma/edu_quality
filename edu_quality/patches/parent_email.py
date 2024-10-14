7758347dceb3560a61dda95b32f640befda0225b
Sankara Subramanian <sankara@walnutedu.in>
2024-10-14 10:16:27 +0000

    fix: uat patch

--- edu_quality/patches/parent_email.py
+++ edu_quality/patches/parent_email.py
@@ -8,4 +8,14 @@ def replace_emails():
     data = frappe.db.get_all("Guardian", fields=["name","email_id"])
     for guardian in data:
         new_email = guardian.email_id.split("@")[0] + "@yopmail.com"
-        frappe.db.set_value("Guardian",guardian.name,"email_id",new_email)
+        frappe.db.set_value("Guardian",guardian.name,{"email_id":new_email,"mobile_number":""})
+    student = frappe.db.get_all("Student", fields=["name","student_email_id"])
+    for st in student:
+        new_email = st.student_email_id.split("@")[0] + "@yopmail.com"
+        frappe.db.set_value("Student",st.name,{
+            "student_email_id":new_email,
+            "student_mobile_number":"",
+            "primary_contact":"",
+            "whatsapp_number":"",
+            })
+        frappe.db.set_value("Student",st.name,)
