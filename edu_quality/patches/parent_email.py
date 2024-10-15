30cbb4f5fd0ed245f554f621b4b64cfac92dd7f6
Sankara Subramanian <sankara@walnutedu.in>
2024-10-15 10:20:02 +0000

    fix: patch condition

--- edu_quality/patches/parent_email.py
+++ edu_quality/patches/parent_email.py
@@ -4,10 +4,10 @@ def execute():
     replace_emails()
 
 def replace_emails():
-    data = frappe.db.get_all("Guardian", fields=["name","email_id"])
+    data = frappe.db.get_all("Guardian", fields=["name","email_address"])
     for guardian in data:
-        new_email = guardian.email_id.split("@")[0] + "@yopmail.com"
-        frappe.db.set_value("Guardian",guardian.name,{"email_id":new_email,"mobile_number":""})
+        new_email = guardian.email_address.split("@")[0] + "@yopmail.com"
+        frappe.db.set_value("Guardian",guardian.name,{"email_address":new_email,"mobile_number":""})
     student = frappe.db.get_all("Student", fields=["name","student_email_id"])
     for st in student:
         new_email = st.student_email_id.split("@")[0] + "@yopmail.com"
