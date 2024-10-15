1224fa7d3f14038f57dcaeeef3d27549b9396759
Sankara Subramanian <sankara@walnutedu.in>
2024-10-15 10:22:30 +0000

    fix: ignore non existenet email

--- edu_quality/patches/parent_email.py
+++ edu_quality/patches/parent_email.py
@@ -6,8 +6,11 @@ def execute():
 def replace_emails():
     data = frappe.db.get_all("Guardian", fields=["name","email_address"])
     for guardian in data:
-        new_email = guardian.email_address.split("@")[0] + "@yopmail.com"
-        frappe.db.set_value("Guardian",guardian.name,{"email_address":new_email,"mobile_number":""})
+        if email:
+            new_email = guardian.email_address.split("@")[0] + "@yopmail.com"
+            frappe.db.set_value("Guardian",guardian.name,{"email_address":new_email,"mobile_number":""})
+        else:
+            frappe.db.set_value("Guardian",guardian.name,"mobile_number","")
     student = frappe.db.get_all("Student", fields=["name","student_email_id"])
     for st in student:
         new_email = st.student_email_id.split("@")[0] + "@yopmail.com"
