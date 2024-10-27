b830a63f4550d95c627fd9ee4e25e3779d1a348a
Sankara Subramanian <sankara@walnutedu.in>
2024-10-27 14:56:55 +0000

    fix: patch

--- edu_quality/patches/image.py
+++ edu_quality/patches/image.py
@@ -4,17 +4,6 @@ def execute():
     pe_list = frappe.db.get_all("Program Enrollment", filters={'academic_year':"2024-2025","docstatus":0},fields=["name", "student"])
     for pe in pe_list:
         try:
-            _file = frappe.get_doc(
-                {
-                    "doctype": "File",
-                    "is_private":1,
-                    "fieldname":"image",
-                    "file_url": "/private/files/"+pe.student +"_2024_2025.jpg",
-                    "attached_to_name": pe.name,
-                    "attached_to_doctype": "Program Enrollment",
-                    "folder": "Home"
-                }
-            )
-            _file.insert(ignore_permissions=True)
+            frappe.db.set_value("Program Enrollment", pe.name, "image","/private/files/"+pe.student +"_2024_2025.jpg")
         except Exception as e:
             pass
