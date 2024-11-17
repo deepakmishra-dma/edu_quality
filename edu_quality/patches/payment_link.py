9698817935238efaf4687a1682eb873bf080ea5a
vikaswalnutedu <vikas@walnutedu.in>
2024-11-17 11:45:55 +0000

    fix: paymnt link

---
+++ edu_quality/patches/payment_link.py
@@ -0,0 +1,16 @@
+import frappe
+from edu_quality.edu_quality.server_scripts.payment_plan import change_payment_plan
+
+
+def execute():
+    fees = frappe.db.get_all("Fees", {"docstatus": 1})
+    fee_advance = frappe.db.get_all("Fee Advance", {"docstatus": 1})
+    for fee in fees:
+        not_paid_filter = {"reference_name": fee.name, "status": ["not in", ["Paid", "Cancelled"]]}
+        if frappe.db.exists("Payment Request", not_paid_filter):
+            change_payment_plan(fee.payment_plan, "Fees", fee.name)
+
+    for fee in fee_advance:
+        not_paid_filter = {"reference_name": fee.name, "status": ["not in", ["Paid", "Cancelled"]]}
+        if frappe.db.exists("Payment Request", not_paid_filter):
+            change_payment_plan(fee.payment_plan, "Fee Advance", fee.name)
