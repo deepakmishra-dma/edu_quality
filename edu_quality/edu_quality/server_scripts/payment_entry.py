3433f71fe2fa17cb73a3445fddfdacf1817b068a
Vikas Moin <vikas@walnutedu.in>
2024-11-25 14:18:10 +0000

    fix: defaulter should convert to Current

--- edu_quality/edu_quality/server_scripts/payment_entry.py
+++ edu_quality/edu_quality/server_scripts/payment_entry.py
@@ -8,3 +8,11 @@ def validate(doc, method=None):
     else:
         letter_head = frappe.get_value("Company", doc.company, 'default_letter_head')
     doc.letter_head = letter_head 
+    filters = {
+        "student": doc.party,
+        "student_status": "Defaulter",
+    }
+    if frappe.db.exists("Student",filters):
+        stud_doc = frappe.get_doc("Student", filters)
+        stud_doc.student_status = "Current student"
+        doc.save(ignore_permissions=True)
