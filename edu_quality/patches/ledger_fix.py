b2a6078b081464ef5c5fca1dfc925b68db7a3141
Sankara Subramanian <sankara@walnutedu.in>
2024-12-19 14:38:57 +0000

    fix:patch

--- edu_quality/patches/ledger_fix.py
+++ edu_quality/patches/ledger_fix.py
@@ -5,15 +5,23 @@ from erpnext.accounts.general_ledger import make_reverse_gl_entries
 
 def execute():
     gl_entry = frappe.qb.DocType("GL Entry")
-    account_list = [{"account":"Fee Advance - UESF","against":"Sales - UESF"}, {"account":"Fee Advance - RESPL","against":"Sales - RESPL"}]
-    for entry in account_list:
-        gl_entries = (
-            frappe.qb.from_(gl_entry)
-            .select("*")
-            .where(gl_entry.voucher_type == entry['account'])
-            .where(gl_entry.against == entry['against'])
-            .where(gl_entry.is_cancelled == 0)
-            .for_update()
-        ).run(as_dict=1)
-        frappe.logger('entry').exception(gl_entries)
-        make_reverse_gl_entries(gl_entries=gl_entries)
+    gl_entries = (
+        frappe.qb.from_(gl_entry)
+        .select("*")
+        .where(gl_entry.voucher_type == "Fee Advance - UESF")
+        .where(gl_entry.against == "Sales - UESF")
+        .where(gl_entry.is_cancelled == 0)
+        .for_update()
+    ).run(as_dict=1)
+    frappe.logger('entry').exception(gl_entries)
+    make_reverse_gl_entries(gl_entries=gl_entries)
+    gl_entries = (
+        frappe.qb.from_(gl_entry)
+        .select("*")
+        .where(gl_entry.voucher_type == "Fee Advance - RESPL")
+        .where(gl_entry.against == "Sales - RESPL")
+        .where(gl_entry.is_cancelled == 0)
+        .for_update()
+    ).run(as_dict=1)
+    frappe.logger('entry').exception(gl_entries)
+    make_reverse_gl_entries(gl_entries=gl_entries)
