f7d95d68d34813f8cb0eeaf4f260a1880a4255d8
Vikas Moin <vikas@walnutedu.in>
2025-01-10 14:20:55 +0000

    fix: quick fixed

--- edu_quality/public/py/walsh/leave.py
+++ edu_quality/public/py/walsh/leave.py
@@ -5,7 +5,6 @@ from edu_quality.edu_quality.server_scripts.student import mark_entry
 
 @frappe.whitelist()
 def add_leave_note(note, status, student, dates):
-    print(dates)
     for date in dates:
         mark_entry(student, "absent_for_" + status, note, date)
         # entry_id_values = {
