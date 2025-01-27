d76647eb0b5c737ecb39f92af9aa1b537dd18d2c
Sankara Subramanian <sankara@walnutedu.in>
2025-01-27 07:48:19 +0000

    fix: user creation hook

--- edu_quality/hooks.py
+++ edu_quality/hooks.py
@@ -187,6 +187,7 @@ doc_events = {
     "Student": {
         "autoname": "edu_quality.public.py.student.autoname",
         "before_insert": "edu_quality.public.py.student.before_insert",
+        "after_insert": "edu_quality.public.py.student.after_insert",
         "before_save": "edu_quality.public.py.student.before_save",
         "on_update": "edu_quality.public.py.student.on_update",
     },
