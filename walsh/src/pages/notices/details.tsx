cb712f0b49de031c5a9e59fb62e43db8b2dda438
Sufiyan Shaikh <shaikhsufiyan174@gmail.com>
2025-02-06 13:23:49 +0000

    added start and archive button on details page and correct calendar and remove addmission link from login

--- walsh/src/pages/notices/details.tsx
+++ walsh/src/pages/notices/details.tsx
@@ -1,15 +1,28 @@
-import {IResourceComponentsProps, useOne} from "@refinedev/core";
-import React from "react";
-import {Box, Stack, Text} from "@mantine/core";
-import {useParams, useSearchParams} from "react-router-dom";
-import {IconCalendar} from "@tabler/icons";
-import {Notice} from "../../components/queries/useNoticeList.ts";
-// import {IconArchive, IconStar} from "@tabler/icons";
+import { IResourceComponentsProps, useOne } from "@refinedev/core";
+import React, { useState } from "react";
+import { Box, Stack, Text } from "@mantine/core";
+import { useParams, useSearchParams } from "react-router-dom";
+// import {   } from "@mantine/core";
+
+import { IconCalendar } from "@tabler/icons";
+import useNoticeList, { Notice } from "../../components/queries/useNoticeList.ts";
+import { IconArchive, IconStar } from "@tabler/icons";
+import { getStudentProfileColor } from "../../components/hooks/useStudentProfileColor.ts";
+import useMarkAsArchived from "../../components/queries/useMarkArchivedMutation.ts";
+import useMarkAsStared from "../../components/queries/useMarkStarMutation.ts";
 
 export const NoticeDetails: React.FC<IResourceComponentsProps> = () => {
   const params = useParams()
   const [queries] = useSearchParams()
-  const {data, isLoading} = useOne<Notice>({
+  const [searchQuery,] = useState('');
+  const { mutateAsync: markAsStared } = useMarkAsStared()
+  const { mutateAsync: markAsArchived } = useMarkAsArchived()
+
+
+  const { data: list, refetch } = useNoticeList({
+
+  });
+  const { data, isLoading } = useOne<Notice>({
     id: params?.id || "", queryOptions: {
       queryKey: ["details", "notices", params?.id, queries?.get("student")],
     }
@@ -18,67 +31,125 @@ export const NoticeDetails: React.FC<IResourceComponentsProps> = () => {
   if (isLoading)
     return <Text align="center" color="dimmed" weight="bold" my={30}>Loading...</Text>
 
-  return <Box>
-    <Box sx={{
-      backgroundColor: '#422e1a',
-      color: 'white',
-      padding: 10
-    }}>
-      <Text weight="bold" size="lg" sx={{
-        width: '100%',
-        padding: '5px 0',
-        fontWeight: 'bold',
-        textAlign: 'center'
+  const useFilteredNotices = (list: any, searchQuery: any) => {
+    if (!list) return [];
+    if (!searchQuery) return list?.data?.message || [];
+    return list?.data?.message?.filter((item: any) => {
+      if (!searchQuery) return true;
+      return (
+        item?.subject?.toLowerCase?.()?.includes(searchQuery.toLowerCase()) ||
+        item?.notice?.toLowerCase?.()?.includes(searchQuery.toLowerCase())
+      );
+    }) || [];
+
+  };
+
+  // Inside your NoticeDetails component
+  const filteredList = useFilteredNotices(list, searchQuery);
+  console.log("filter ", filteredList)
+
+  if (filteredList?.length > 0) {
+    return <Box>
+      <Box sx={{
+        backgroundColor: '#422e1a',
+        color: 'white',
+        padding: 10
       }}>
-        {data?.data?.subject || '-'}
-      </Text>
-      <Stack h={35} sx={{
-        flexDirection: 'row',
-        // justifyContent: 'space-between',
-        paddingTop: 5,
-        paddingBottom: 5,
-        borderTop: '1px solid #eee',
-        gap: 10,
-        color: '#fdc426',
-        justifyContent: 'center'
-      }}>
-        <Stack align="center" justify="center" pt={4} pr={10} sx={{
-          display: 'inline-block',
-          whiteSpace: 'nowrap',
-          fontSize: 13,
-          borderRight: '1px solid #eee'
-        }}>{data?.data?.student_first_name}</Stack>
-        <Stack align="center" justify="center" py={4} sx={{
-          display: 'inline-flex',
-          // justifyContent: 'center',
-          flexDirection: 'row',
-          // alignItems: 'center',
-          // borderRadius: 5,
-          whiteSpace: 'nowrap',
-          fontSize: 13,
-          gap: 5
+        <Text weight="bold" size="lg" sx={{
+          width: '100%',
+          padding: '5px 0',
+          fontWeight: 'bold',
+          textAlign: 'center'
         }}>
-          <IconCalendar size={15}/>
-          <span style={{paddingTop: 5}}>
-                  {new Date(data?.data.creation).toLocaleDateString()?.replace(/\//g, '-') || '-'}
-                </span>
+          {data?.data?.subject || '-'}
+        </Text>
+        <Stack h={35} sx={{
+          flexDirection: 'row',
+          // justifyContent: 'space-between',
+          paddingTop: 5,
+          paddingBottom: 5,
+          borderTop: '1px solid #eee',
+          gap: 10,
+          color: '#fdc426',
+          justifyContent: 'center'
+        }}>
+          <Stack align="center" justify="center" pt={4} pr={10} sx={{
+            display: 'inline-block',
+            whiteSpace: 'nowrap',
+            fontSize: 13,
+            borderRight: '1px solid #eee'
+          }}>{data?.data?.student_first_name}</Stack>
+          <Stack align="center" justify="center" py={4} sx={{
+            display: 'inline-flex',
+            // justifyContent: 'center',
+            flexDirection: 'row',
+            // alignItems: 'center',
+            // borderRadius: 5,
+            whiteSpace: 'nowrap',
+            fontSize: 13,
+            gap: 5
+          }}>
+            <IconCalendar size={15} />
+            <span style={{ paddingTop: 5 }}>
+              {new Date(data?.data.creation).toLocaleDateString()?.replace(/\//g, '-') || '-'}
+            </span>
+            {
+              filteredList?.map?.((item: any) => (
+                <>
+                  <IconStar
+                    style={{
+                      marginBottom: 1,
+                      position: "absolute",
+                      right: 45
+                    }}
+                    size={30}
+                    fill={item.is_stared ? getStudentProfileColor(item.student, data?.data?.message || []) : 'white'}
+                    color={getStudentProfileColor(item.student, data?.data?.message || [])}
+                    stroke={1}
+                    onClick={() => {
+                      markAsStared({ notice: item.name, student: item.student, stared: !item.is_stared })
+                        .then(() => refetch())
+                    }}
+                  />
+                  <IconArchive
+                    style={{
+                      marginBottom: 1,
+                      position: "absolute",
+                      right: 8
+                    }}
+                    size={30}
+                    color={item.is_archived ? "white" : getStudentProfileColor(item.student, data?.data?.message || [])}
+                    stroke={1}
+                    fill={item.is_archived ? getStudentProfileColor(item.student, data?.data?.message || []) : 'white'}
+                    onClick={() => {
+                      markAsArchived({ notice: item.name, student: item.student, archived: !item.is_archived })
+                        .then(() => refetch())
+                    }}
+                  />
+                </>
+              ))
+            }
+          </Stack>
         </Stack>
-      </Stack>
+      </Box>
+      <Box sx={{
+        overflow: 'auto',
+        width: '100%',
+        padding: 10
+      }}>
+        {
+          data?.data?.is_raw_html ? <>
+            <div dangerouslySetInnerHTML={{ __html: data?.data?.notice || "" }}></div>
+          </> : <>
+            <link href="https://cdn.jsdelivr.net/npm/quill@2.0.0-beta.0/dist/quill.snow.css" rel="stylesheet" />
+            <link href="https://cdn.jsdelivr.net/npm/quill@2.0.0-beta.0/dist/quill.bubble.css" rel="stylesheet" />
+            <div className="ql-editor" dangerouslySetInnerHTML={{ __html: data?.data?.notice || "" }}></div>
+          </>
+        }
+      </Box>
     </Box>
-    <Box sx={{
-      overflow: 'auto',
-      width: '100%',
-      padding: 10
-    }}>
-      {
-        data?.data?.is_raw_html ? <>
-          <div dangerouslySetInnerHTML={{__html: data?.data?.notice || ""}}></div>
-        </> : <>
-          <link href="https://cdn.jsdelivr.net/npm/quill@2.0.0-beta.0/dist/quill.snow.css" rel="stylesheet"/>
-          <link href="https://cdn.jsdelivr.net/npm/quill@2.0.0-beta.0/dist/quill.bubble.css" rel="stylesheet"/>
-          <div className="ql-editor" dangerouslySetInnerHTML={{__html: data?.data?.notice || ""}}></div>
-        </>
-      }
-    </Box>
-  </Box>
+  }
+  return <Text align="center" color="dimmed" weight="bold" my={30}>
+    No Notice Found
+  </Text>
 };
