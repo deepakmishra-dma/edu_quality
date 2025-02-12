a490c4a961bd2d89830cd64e039bbb21fd280d14
Sankara Subramanian <sankara@walnutedu.in>
2025-02-12 13:33:05 +0000

    fix: class name in patch

--- edu_quality/patches/student_applicant_classes.py
+++ edu_quality/patches/student_applicant_classes.py
@@ -9,17 +9,20 @@ school_map = {
 def execute():
 
     junior_kg_applicants = frappe.db.get_all("Student Applicant",filters=[["Student Applicant","application_status","!=","Admitted"],["Student Applicant","program","like","%Junior KG-Walnut School%"]])
-    process_data(junior_kg_applicants)
+    process_data(junior_kg_applicants,"Junior KG-")
     senior_kg_applicants = frappe.db.get_all("Student Applicant",filters=[["Student Applicant","application_status","!=","Admitted"],["Student Applicant","program","like","%Senior KG-Walnut School%"]])
-    process_data(senior_kg_applicants)
+    process_data(senior_kg_applicants,"Senior KG-")
     nursery_applicants = frappe.db.get_all("Student Applicant",filters=[["Student Applicant","application_status","!=","Admitted"],["Student Applicant","program","like","%Nursery-Walnut School%"]])
-    process_data(nursery_applicants)
-    
-def process_data(applicants):
+    process_data(nursery_applicants,"Nursery-")
+
+def process_data(applicants,class_name):
     for applicant in applicants:
             doc = frappe.get_doc("Student Applicant",applicant.name)
             doc.school = school_map[doc.school]
-            doc.program = "Junior KG-" + doc.school
+            doc.program = class_name + doc.school
             doc.fee_structure = ""
             doc.academic_year = "2024-2025"
             doc.save(ignore_permissions=True)
+
+
+
