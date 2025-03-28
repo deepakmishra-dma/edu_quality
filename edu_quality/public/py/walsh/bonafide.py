a27ad6c8908859dbbd8b9833bf5967e4f486c783
badal8381 <badalsahani8381@gmail.com>
2025-03-28 15:45:21 +0000

    feat: bonafide certificate api

--- edu_quality/public/py/walsh/bonafide.py
+++ edu_quality/public/py/walsh/bonafide.py
@@ -1,11 +1,58 @@
 import frappe
-from nextai.funnel.custom_trigger import trigger_event
+from frappe.utils.file_manager import save_file
+from edu_quality.edu_quality.server_scripts.utils import current_academic_year
 
 
 @frappe.whitelist(allow_guest=True)
 def send_bonafide(student_id):
-    # from nextai.funnel.custom_trigger import trigger_event
-    student = frappe.get_doc("Student", student_id)
-    trigger_event(doc=student, event_name="bonafide_certificate")
-    
-    
+    if frappe.db.exists(
+        "Bonafide Certificate",
+        {"student": student_id, "academic_year": current_academic_year()},
+    ):
+        bonafide_pdf = frappe.get_doc(
+            "Bonafide Certificate", {"student": student_id}
+        ).bonafide_pdf
+        pdf_url = frappe.utils.get_url(bonafide_pdf)
+        return pdf_url
+    else:
+        pdf_content = frappe.get_print(
+            "Student", student_id, print_format="Bonafide Certificate"
+        )
+        acad_year = current_academic_year()
+
+        doc = frappe.get_doc(
+            {
+                "doctype": "Bonafide Certificate",
+                "student": student_id,
+                "academic_year": acad_year,
+            }
+        )
+        doc.insert()
+
+        saved_file = save_file(
+            fname=f"{student_id}.pdf",
+            content=pdf_content,
+            dt="Bonafide Certificate",
+            dn=doc.name,
+            df="bonafide_pdf",
+            folder="bonafide_certificates"
+        )
+
+        guardians = frappe.get_value("Student", student_id, "guardians")
+
+        guardian_email = [i.guardian_name for i in guardians]
+        guardian = frappe.get_all(
+            doctype="Guardian",
+            fields=["email_address"],
+            filters=[["guardian_name", "in", guardian_email]],
+        )
+        recipients = [i.email_address for i in guardian]
+
+        frappe.sendmail(
+            recipients=recipients,
+            subject="Bonafide Certificate",
+            message="Please find attached Bonafide Certificate",
+            attachments=[saved_file.file_url],
+        )
+        pdf_url = frappe.utils.get_url(saved_file.file_url)
+        return pdf_url
