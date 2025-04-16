65b7efe67c2eb5c96d11a1d7b3a1c7e926b0491f
Vikas Moin <vikas@walnutedu.in>
2025-04-16 14:27:43 +0000

    feat: roll number allocation

--- edu_quality/hooks.py
+++ edu_quality/hooks.py
@@ -173,6 +173,7 @@ doc_events = {
         ],
         "on_trash": "edu_quality.public.py.fee.remove_program_enrollment",
         "on_update_after_submit": "edu_quality.public.py.fee.update_program_enrollment",
+        "on_cancel": "edu_quality.edu_quality.server_scripts.program_enrollment.on_cancel",
     },
     "Contact": {
         "before_validate": "edu_quality.overrides_hooks.contact.before_validate"
