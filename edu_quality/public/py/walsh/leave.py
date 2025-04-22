0808dc774c76b7d96eb5f974e7f25f806cac5788
aguyran <aguyran@gmail.com>
2025-04-22 08:52:51 +0000

    fix absent issues

--- edu_quality/public/py/walsh/leave.py
+++ edu_quality/public/py/walsh/leave.py
@@ -6,7 +6,7 @@ from edu_quality.edu_quality.server_scripts.student import mark_entry
 @frappe.whitelist()
 def add_leave_note(note, status, student, dates):
     for date in dates:
-        mark_entry(student, "absent_for_" + status, note, date)
+        mark_entry(student, "absent", note, date)
     return {
         "success": True,
         "message": "Note Saved",
