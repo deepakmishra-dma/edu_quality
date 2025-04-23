882deeca5733b3f3d657e1f04d2b98a7f1b06259
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2025-04-23 09:56:10 +0000

    fix: indentation error

--- edu_quality/edu_quality/overrides/hd_ticket.py
+++ edu_quality/edu_quality/overrides/hd_ticket.py
@@ -3,18 +3,18 @@ from helpdesk.helpdesk.doctype.hd_ticket.hd_ticket import HDTicket
 
 class CustomHDTicket(HDTicket):
     def on_communication_update(self, c):
-		# If communication is incoming, then it is a reply from customer, and ticket must
-		# be reopened.
-		if c.sent_or_received == "Received":
-			self.status = "Open"
-		# If communication is outgoing, it must be a reply from agent
-		if c.sent_or_received == "Sent":
-			# Set first response date if not set already
-			self.first_responded_on = (
-				self.first_responded_on or frappe.utils.now_datetime()
-			)
-		# Fetch description from communication if not set already. This might not be needed
-		# anymore as a communication is created when a ticket is created.
-		self.description = self.description or c.content
-		# Save the ticket, allowing for hooks to run.
-		self.save(ignore_permissions=True)
+        # If communication is incoming, then it is a reply from customer, and ticket must
+        # be reopened.
+        if c.sent_or_received == "Received":
+            self.status = "Open"
+        # If communication is outgoing, it must be a reply from agent
+        if c.sent_or_received == "Sent":
+            # Set first response date if not set already
+            self.first_responded_on = (
+                self.first_responded_on or frappe.utils.now_datetime()
+            )
+        # Fetch description from communication if not set already. This might not be needed
+        # anymore as a communication is created when a ticket is created.
+        self.description = self.description or c.content
+        # Save the ticket, allowing for hooks to run.
+        self.save(ignore_permissions=True)
