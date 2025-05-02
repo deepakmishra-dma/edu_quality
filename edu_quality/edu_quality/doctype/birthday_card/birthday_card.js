3ce2ec4e032c2c4d7f1aa1b8f2934141fd65ab6c
aguyran <aguyran@gmail.com>
2025-05-02 13:46:00 +0000

    add item comment,real date show blank, doc changes

--- edu_quality/edu_quality/doctype/birthday_card/birthday_card.js
+++ edu_quality/edu_quality/doctype/birthday_card/birthday_card.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Hybrowlabs Technologies and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Birthday Celebration", {
+frappe.ui.form.on("Birthday Card", {
     refresh(frm) {
         if (!frm.doc.__islocal) {
             frm.add_custom_button("Take Photos", async () => {
