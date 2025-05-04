a3a5b46c9b8fe6364b01bfdbd2d3e1bf2486e750
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2025-05-04 09:08:45 +0000

    fix: filters

--- edu_quality/fees/page/payment_redirect/__init__.py
+++ edu_quality/fees/page/payment_redirect/__init__.py
@@ -87,7 +87,7 @@ def get_payment_details(**kwargs):
 
     #redirect to deposit if present
     if frappe.db.exists("Payment Request",[["Payment Request","status","=","Initiated"],["Payment Request","payment_term","is","not set"],["Payment Request","reference_name","=",payment_request.reference_name]]):
-        request = frappe.db.get_value("Payment Request",filters=[["Payment Request","status","=","Initiated"],["Payment Request","payment_term","is","not set"],["Payment Request","reference_name","=",payment_request.reference_name]],"payment_hash")
+        request = frappe.db.get_value("Payment Request",[["Payment Request","status","=","Initiated"],["Payment Request","payment_term","is","not set"],["Payment Request","reference_name","=",payment_request.reference_name]],"payment_hash")
         return {'redirect': frappe.utils.get_url()+"/payment?payment_request="+request}
     fees = frappe.get_doc(payment_request.reference_doctype, payment_request.reference_name)
     breakup = []
