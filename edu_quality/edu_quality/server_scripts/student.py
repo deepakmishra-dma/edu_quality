c10197ec3fe13c7775fe235c2268399b65cb2d0c
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2025-05-30 18:41:05 +0000

    feat: TASK-01700 - Case 1

--- edu_quality/edu_quality/server_scripts/student.py
+++ edu_quality/edu_quality/server_scripts/student.py
@@ -2,6 +2,75 @@ import frappe
 
 from frappe.utils import getdate
 import datetime
+from erpnext.accounts.doctype.payment_request.payment_request import get_gateway_details
+from erpnext.accounts.party import get_party_bank_account
+from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
+    get_accounting_dimensions,
+)
+
+
+@frappe.whitelist()
+def validate_bank_account(student):
+    return frappe.db.exists("Bank Account", {"party": student})
+
+@frappe.whitelist()
+def cancel_student(student,academic_year,fee_collection):
+    try:
+        if frappe.db.exists("Program Enrollment",{'student':student,'academic_year':academic_year,'docstatus':1}):
+            frappe.db.set_value("Program Enrollment",{'student':student,'academic_year':academic_year,'docstatus':1},'docstatus',2)
+        frappe.db.set_value("Student",student,'enabled',0)
+        frappe.db.set_value("Student",student,"student_status","Cancelled")
+        fees_list = frappe.db.get_all("Fees",filters={'docstatus':1,'student':student})
+        for fee in fees_list:
+            if frappe.db.exists("Fee Component",[['parent','=',fee.name],['fees_category','like','%DEPOSIT%']]):
+                deposit = frappe.db.get_value("Fee Component",[['parent','=',fee.name],['fees_category','like','%DEPOSIT%']],'amount')
+                refund_deposit(student,fee.name,deposit)
+        return 1
+    except Exception as e:
+        frappe.logger("Cancel").exception(e)
+        return 0
+
+def refund_deposit(student,fee,amount):
+    gateway_account = get_gateway_details({}) or frappe._dict()
+    pr = frappe.new_doc("Payment Request")
+    ref_doc = frappe.get_doc("Fees",fee)
+    bank_account = (
+        get_party_bank_account("Student", student)
+    )
+    pr.update(
+        {
+            "payment_gateway_account": gateway_account.get("name"),
+            "payment_gateway": gateway_account.get("payment_gateway"),
+            "payment_account": gateway_account.get("payment_account"),
+            "payment_channel": gateway_account.get("payment_channel"),
+            "payment_request_type": "Outward",
+            "currency": "INR",
+            "grand_total": amount,
+            "email_to": student+"@walnutedu.in",
+            "subject": "Deposit Refund For for {0}".format(student),
+            "message": "Deposit Refund",
+            "reference_doctype": "Fees",
+            "reference_name": fee,
+            "party_type": "Student",
+            "party": student,
+            "bank_account": bank_account,
+            "company": ref_doc.get("company"),
+        }
+    )
+
+    # Update dimensions
+    pr.update(
+        {
+            "cost_center": ref_doc.get("cost_center"),
+            "project": ref_doc.get("project"),
+        }
+    )
+
+    for dimension in get_accounting_dimensions():
+        pr.update({dimension: ref_doc.get(dimension)})
+
+    pr.insert(ignore_permissions=True)
+    pr.submit()
 
 
 @frappe.whitelist()
