800afa636a2190f6e5cc3d25cf2305a953c614cf
badal8381 <badalsahani8381@gmail.com>
2025-07-14 11:20:52 +0000

    TASK-01685: print mutliple birthday card at once from listview

--- edu_quality/edu_quality/doctype/birthday_card/birthday_card.js
+++ edu_quality/edu_quality/doctype/birthday_card/birthday_card.js
@@ -14,29 +14,6 @@ frappe.ui.form.on("Birthday Card", {
                 })
 
             })
-            frm.add_custom_button("Print Birthday Card", async function () {
-                let pdfUrl
-                try {
-                    const headers = new Headers()
-                    headers.append('X-Frappe-CSRF-Token', frappe.csrf_token)
-                    headers.append('Content-Type', 'application/json')
-                    const payload = { "birthday_card": frm.doc.name, type: "POST" }
-                    const generated = await fetch(`/api/method/edu_quality.edu_quality.doctype.birthday_card.birthday_card.print_birthday_card`, {
-                        method: 'POST',
-                        headers: headers, body: JSON.stringify(payload)
-                    })
-                    const file = await generated.blob()
-                    pdfUrl = URL.createObjectURL(file);
-                    window.open(pdfUrl, '_blank');
-                } catch (e) {
-                    console.error(e)
-                }
-                finally {
-                    if (pdfUrl) {
-                        URL.revokeObjectURL(pdfUrl)
-                    }
-                }
-            })
         }
     },
 });
