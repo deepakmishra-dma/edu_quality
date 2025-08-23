3d5c0b2ea92e31d7408e0ddb3c2ef02f14774d07
badal8381 <badalsahani8381@gmail.com>
2025-08-23 08:05:02 +0000

    TASK-01749: fire email to the admin group of the school about the early pick up

--- edu_quality/attendance/doctype/attendance_entry/attendance_entry.py
+++ edu_quality/attendance/doctype/attendance_entry/attendance_entry.py
@@ -1,8 +1,33 @@
 # Copyright (c) 2024, Hybrowlabs Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class AttendanceEntry(Document):
-	pass
+
+    def on_update(self):
+        self.send_email_to_admins()
+
+    def send_email_to_admins(self):
+        admin_emails = self.get_admin_emails()
+        if admin_emails:
+            timestamp = frappe.get_value(
+                "Absent and Delay",
+                {"parent": self.name, "status": "early_pickup"},
+                "timestamp",
+            )
+            message = f"Attendance Entry has been created/updated for student {self.student_name}({self.student}) of class {getattr(self, 'class')} on {timestamp}"
+            frappe.sendmail(
+                recipients=admin_emails, subject="Attendance Entry", message=message
+            )
+
+    def get_admin_emails(self):
+        school = frappe.get_value("Program", getattr(self, "class"), "school")
+        admin_group = frappe.get_value("School", school, "admin_group")
+        return frappe.get_all(
+            "Email Group Member",
+            filters={"email_group": admin_group},
+            pluck="email",
+        )
