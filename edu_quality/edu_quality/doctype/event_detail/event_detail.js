64a9fd2b5206de855a5087136816fb2a6adb7a5d
badal8381 <badalsahani8381@gmail.com>
2025-09-12 08:45:41 +0000

    TASK-01992: add button to add student in participating

--- edu_quality/edu_quality/doctype/event_detail/event_detail.js
+++ edu_quality/edu_quality/doctype/event_detail/event_detail.js
@@ -2,7 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Event Detail", {
-    add_students: (frm) => { showAddStudent(frm); },
+    add_allowed_students: (frm) => { showAddStudent(frm); },
+    add_to_participating: (frm) => { addToParticipating(frm); },
 
     refresh(frm) {
         frm.add_custom_button('Send Registration Link', () => {
@@ -18,98 +19,15 @@ function showAddStudent(frm) {
         title: 'Add Students',
         fields: [
             {
-                label: 'By Class',
-                fieldname: 'by_class',
-                fieldtype: 'Check',
-                default: 0,
-                onchange: function () {
-                    if (d.get_value('by_class')) {
-                        d.set_df_property('student_status', 'hidden', 0);
-                        d.set_df_property('program', 'hidden', 0);
-                        d.set_df_property('division', 'hidden', 1);
-                        d.set_df_property('reference_number', 'hidden', 1);
-                        d.set_value('by_division', 0);
-                        d.set_value('by_reference_number', 0);
-                        d.refresh();
-                    } else {
-                        d.set_df_property('program', 'hidden', 1);
-                        d.set_df_property('student_status', 'hidden', 1);
-                    }
-                }
-            },
-            {
-                fieldtype: "Column Break",
-            },
-            {
-                label: 'By Division',
-                fieldname: 'by_division',
-                fieldtype: 'Check',
-                default: 0,
-                onchange: function () {
-                    if (d.get_value('by_division')) {
-                        d.set_df_property('division', 'hidden', 0);
-                        d.set_df_property('program', 'hidden', 1);
-                        d.set_df_property('student_status', 'hidden', 1);
-                        d.set_df_property('reference_number', 'hidden', 1);
-                        d.set_value('by_class', 0);
-                        d.set_value('by_reference_number', 0);
-                        d.refresh();
-                    } else {
-                        d.set_df_property('division', 'hidden', 1);
-                    }
-                }
-            },
-            {
-                fieldtype: "Column Break",
-            },
-            {
-                label: 'By Reference Number',
-                fieldname: 'by_reference_number',
-                fieldtype: 'Check',
-                default: 0,
-                onchange: function () {
-                    if (d.get_value('by_reference_number')) {
-                        d.set_df_property('reference_number', 'hidden', 0);
-                        d.set_df_property('division', 'hidden', 1);
-                        d.set_df_property('program', 'hidden', 1);
-                        d.set_df_property('student_status', 'hidden', 1);
-                        d.set_value('by_class', 0);
-                        d.set_value('by_division', 0);
-                        d.refresh();
-                    } else {
-                        d.set_df_property('reference_number', 'hidden', 1);
-                    }
-                }
-            },
-            {
-                fieldtype: "Section Break",
-            },
-            {
                 label: 'Enter Reference Numbers Separated by Comma',
                 fieldname: 'reference_number',
                 fieldtype: 'Small Text',
-                hidden: 1,
-            },
-            {
-                label: 'Class',
-                fieldname: 'program',
-                fieldtype: 'Table MultiSelect',
-                options: 'Classes',
-                hidden: 1,
             },
             {
                 label: 'Student Status',
                 fieldname: 'student_status',
                 fieldtype: 'Select',
                 options: 'New student\nCurrent student\nCancelled\nNot attending\nDefaulter\nAlumni',
-                hidden: 1,
-            },
-            {
-                label: 'Division',
-                fieldname: 'division',
-                fieldtype: 'Link',
-                options: 'Student Group',
-                hidden: 1,
             }
         ],
         size: 'large',
@@ -128,15 +46,17 @@ async function AddStudent(values, frm) {
         doc: frm.doc,
         method: 'get_students',
         args: {
-            program: values.program,
+            school: frm.doc.school,
             student_status: values.student_status,
-            division: values.division,
             reference_number: values.reference_number,
-            school: frm.doc.school
         }
     })
 
     let allowed_students = frm.get_field("allowed_students");
+    if (students.message.length == 0) {
+        frappe.msgprint("No Student Found");
+        return;
+    }
     students.message.forEach(student => {
         let new_row = allowed_students.grid.add_new_row();
         new_row.student = student.name;
@@ -159,4 +79,25 @@ function sendRegistrationLink(frm) {
             }
         }
     });
+}
+
+
+function addToParticipating(frm) {
+    let allowed_students = frm.doc.allowed_students;
+    let participating_students = frm.doc.participating_students;
+
+    allowed_students.forEach(student => {
+        if (student.__checked) {
+            let already_participating = participating_students.some(participant => participant.student === student.student);
+            
+            if (!already_participating) {
+                let new_row = frm.add_child("participating_students");
+                new_row.student = student.student;
+                new_row.student_name = student.student_name;
+            }
+        }
+    });
+
+    frm.refresh_field("participating_students");
+    frm.refresh();
 }
--- edu_quality/edu_quality/doctype/event_detail/event_detail.py
+++ edu_quality/edu_quality/doctype/event_detail/event_detail.py
@@ -11,8 +11,8 @@ class EventDetail(Document):
         self.update_classes()
 
     def update_classes(self):
-        cal_doc = frappe.get_doc("Calender", self.calendar_event_name)
-        for cls in cal_doc.classes_applicable_to:
+        event = frappe.get_doc("Event", self.event)
+        for cls in event.custom_classes:
             if not frappe.db.exists(
                 "Classes", {"parent": self.name, "class": cls.get("class")}
             ):
@@ -20,29 +20,9 @@ class EventDetail(Document):
 
     @frappe.whitelist()
     def get_students(self, args):
-        program = args.get("program")
-        division = args.get("division")
         refno = args.get("reference_number")
+        student_status = args.get("student_status")
         students = []
-        if program:
-            student_status = args.get("student_status")
-            program = [program.get("class") for program in program]
-            students = frappe.get_all(
-                "Student",
-                filters={"program": ["in", program], "student_status": student_status},
-                fields=["name", "student_name"],
-            )
-        if division:
-            students = frappe.db.sql(
-                """
-                SELECT student.name, student.student_name FROM `tabStudent` as student
-                JOIN `tabProgram Enrollment` as program_enrollment ON student.name = program_enrollment.student
-                WHERE program_enrollment.student_group = %s
-                AND program_enrollment.custom_status != 'Cancelled'
-            """,
-                division,
-                as_dict=True,
-            )
         if refno:
             school = args.get("school")
             refno_list = refno.split(",")
@@ -51,7 +31,7 @@ class EventDetail(Document):
                 filters={
                     "reference_number": ["in", refno_list],
                     "school": school,
-                    "student_status": ["!=", "Cancelled"],
+                    "student_status": student_status,
                 },
                 fields=["name", "student_name"],
             )
