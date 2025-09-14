6929f56dafd91cd476350d9a01133a27e629fa9c
aguyran <aguyran@gmail.com>
2025-09-14 11:28:58 +0000

    fix migration error

--- edu_quality/edu_quality/doctype/assessment_processing/assessment_processing.py
+++ edu_quality/edu_quality/doctype/assessment_processing/assessment_processing.py
@@ -17,19 +17,23 @@ def process_exams(acad_year, school, exam_name, program, div=[], reference_numbe
     div_query = assess_plan_qb.div.notnull()
     if div:
         div_query = assess_plan_qb.isin(div)
-    valid_plans = frappe.qb.from_(assess_plan_qb).inner_join(assess_plan_cr_qb).on(
-        assess_plan_cr_qb.parent == assess_plan_qb.name
-    ).where(
-        (assess_plan_qb.academic_year == acad_year)
-        & (assess_plan_qb.school == school)
-        & (assess_plan_qb.assessment_group == exam_name)
-        & (div_query)
-    ).select(
-        assess_plan_qb.name,
-        assess_plan_cr_qb.custom_scale,
-        assess_plan_qb.assessment_criteria,
+    valid_plans = (
+        frappe.qb.from_(assess_plan_qb)
+        .inner_join(assess_plan_cr_qb)
+        .on(assess_plan_cr_qb.parent == assess_plan_qb.name)
+        .where(
+            (assess_plan_qb.academic_year == acad_year)
+            & (assess_plan_qb.school == school)
+            & (assess_plan_qb.assessment_group == exam_name)
+            & (div_query)
+        )
+        .select(
+            assess_plan_qb.name,
+            assess_plan_cr_qb.custom_scale,
+            assess_plan_qb.assessment_criteria,
+        )
     )
-    
+
     plan_hash = gen_assess_cr_hash(valid_plans.run(as_dict=True))
     assess_result_qb = frappe.qb.DocType("Assessment Result")
 
@@ -42,6 +46,8 @@ def gen_assess_cr_hash(assess_plans):
             plan_hash[name] = [plan]
         else:
             plan_hash[name].append(plan)
-	return plan_hash
+
+
+# return plan_hash
 def calculate_exam_marks():
     pass
