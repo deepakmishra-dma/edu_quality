b2a507cd8925d0180fd05ae0e8fc9020c5ae5cff
Sufiyan Shaikh <shaikhsufiyan174@gmail.com>
2025-09-21 08:43:21 +0000

    added cmapc creation tool on workflow

--- walnutMGR/src/main.tsx
+++ walnutMGR/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.js'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
+import reactToWebComponent from "react-to-webcomponent";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// import { Index } from "./components/pages/index.js";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+customElements.define(
+  "cmap-creation-tool",
+  reactToWebComponent(App, React, ReactDOM)
+);
