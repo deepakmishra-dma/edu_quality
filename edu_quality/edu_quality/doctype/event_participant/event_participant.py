cfd28e8f8b0e7e1ead04ea7ca2e5a77803fa57d3
badal8381 <badalsahani8381@gmail.com>
2025-11-16 12:16:27 +0000

    TASK-01992: add accept payment in event participant

--- edu_quality/edu_quality/doctype/event_participant/event_participant.py
+++ edu_quality/edu_quality/doctype/event_participant/event_participant.py
@@ -10,7 +10,11 @@ class EventParticipant(Document):
     def get_participant_details(self):
         return frappe.json.loads(self.data)
 
-    def after_insert(self):
+    def on_submit(self):
+        if self.outstanding_amount < 0:
+            self.outstanding_amount = 0
+        if not self.paid or self.outstanding_amount != 0:
+            frappe.throw("Payment is pending")
         if not frappe.db.exists(
             "Student Data", {"student": self.student, "parent": self.event_detail}
         ):
@@ -22,6 +26,18 @@ class EventParticipant(Document):
             participant.parenttype = "Event Detail"
             participant.parentfield = "participating_students"
             participant.save(ignore_permissions=True)
+
+    def validate_payment(self, data=None):
+        if data:
+            amount = data.get("amount")
+            self.paid = 1
+            self.outstanding_amount -= amount
+            self.paid_amount = amount
+            self.save(ignore_permissions=True)
+            self.submit()
+            return {"status": "success", "message": "Payment Successful"}
+        else:
+            return {"status": "error", "message": "Payment data not found"}
 
 
 @frappe.whitelist()
