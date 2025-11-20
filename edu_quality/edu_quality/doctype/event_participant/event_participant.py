14197ffe8dd1783d307c6aee30e4f2b66725114d
badal8381 <badalsahani8381@gmail.com>
2025-11-20 09:01:33 +0000

    TASK-01992: Event Participant field changes

--- edu_quality/edu_quality/doctype/event_participant/event_participant.py
+++ edu_quality/edu_quality/doctype/event_participant/event_participant.py
@@ -12,10 +12,11 @@ class EventParticipant(Document):
         return frappe.json.loads(self.data)
 
     def on_submit(self):
-        if self.outstanding_amount < 0:
-            self.outstanding_amount = 0
-        if not self.paid or self.outstanding_amount != 0:
-            frappe.throw("Payment is pending")
+        if self.payment_required:
+            if self.outstanding_amount < 0:
+                self.outstanding_amount = 0
+            if not self.paid or self.outstanding_amount != 0:
+                frappe.throw("Payment is pending")
         if not frappe.db.exists(
             "Student Data", {"student": self.student, "parent": self.event_detail}
         ):
@@ -31,7 +32,7 @@ class EventParticipant(Document):
     def validate_payment(self, data=None):
         if data:
             amount = data.get("amount")
-            self.paid = 1
+            self.payment_required = self.paid = 1
             self.outstanding_amount -= amount
             self.paid_amount = amount
             self.save(ignore_permissions=True)
