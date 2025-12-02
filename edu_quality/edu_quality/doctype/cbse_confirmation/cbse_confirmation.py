669221caf9198c83f710396e488491be0b7cbfc9
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2025-12-02 11:30:44 +0000

    feat: bulk generation, user auth, auto login

--- edu_quality/edu_quality/doctype/cbse_confirmation/cbse_confirmation.py
+++ edu_quality/edu_quality/doctype/cbse_confirmation/cbse_confirmation.py
@@ -3,7 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
-
+from edu_quality.edu_quality.server_scripts.utils import set_user_permission, set_form_user
 
 class CBSEConfirmation(Document):
     """
@@ -12,8 +12,14 @@ class CBSEConfirmation(Document):
     """
     def after_insert(self):
         """Called after the document is inserted. Triggers autofill of student details."""
-        self.autofill_student_details()
-        self.form_hash = frappe.generate_hash(self.name, length=20)
+        self.autofill_student_details()        
+        self.set_guardian_permission(self.form_user)
+        frappe.db.set_value('CBSE Confirmation', self.name, 'form_hash', frappe.generate_hash(self.name, length=20))
+        self.reload()
+    
+    def set_guardian_permission(self, user):
+        """Set permission for the guardian user to access the CBSE Confirmation document."""
+        set_user_permission(user, "CBSE Confirmation", self.name)
 
     def autofill_student_details(self):
         """
@@ -51,6 +57,14 @@ class CBSEConfirmation(Document):
     def _construct_full_name(self, first_name, middle_name, last_name):
         """
         Constructs a full name from first, middle, and last names.
+        
+        Args:
+            first_name (str): The first name of the person.
+            middle_name (str): The middle name of the person.
+            last_name (str): The last name of the person.
+        
+        Returns:
+            str: The full name constructed from the given components.
         """
         parts = [first_name]
         if middle_name:
@@ -62,6 +76,9 @@ class CBSEConfirmation(Document):
     def _update_guardian_details(self, guardian):
         """
         Updates the document with guardian details based on the relation (Father/Mother).
+        
+        Args:
+            guardian (Guardian): The Guardian document object.
         """
         relation = guardian.relation.lower()
         doc = frappe.get_doc("Guardian", guardian.guardian)
@@ -82,6 +99,60 @@ class CBSEConfirmation(Document):
         
         self.update(details)
 
-@frappe.whitelist()
+@frappe.whitelist(allow_guest=True)
 def get_form_details(hash):
-    pass
+    """
+    Retrieves the CBSE Confirmation document details based on the provided hash.
+    
+    Args:
+        hash (str): The unique hash associated with the CBSE Confirmation document.
+    
+    Returns:
+        dict: A dictionary containing the form user details.
+    
+    Raises:
+        frappe.PermissionError: If the CBSE Confirmation document with the provided hash is not found.
+    """
+    print(hash)
+    if not frappe.db.exists("CBSE Confirmation", {'form_hash': hash}):
+        frappe.throw("Form Not Found!")
+    docname = frappe.db.get_value("CBSE Confirmation", {'form_hash': hash}, 'name')
+    return set_form_user("CBSE Confirmation", docname)
+
+
+@frappe.whitelist()
+def generate_confirmations(program):
+    """
+    Generates CBSE Confirmation documents for students enrolled in the given program.
+    
+    Args:
+        program (str): The name of the program for which CBSE Confirmations need to be generated.
+    
+    Returns:
+        dict: A dictionary containing the status (success or failure) and an error message (if applicable).
+    """
+    try:
+        frappe.enqueue(student_confirmation_generation, program=program, queue='long')
+        return {'status': 1}
+    except Exception as e:
+        return {'status': 0, 'error': str(e)}
+    
+
+
+def student_confirmation_generation(program):
+    """
+    Helper function to generate CBSE Confirmation documents for students enrolled in the given program.
+    
+    Args:
+        program (str): The name of the program for which CBSE Confirmations need to be generated.
+    """
+    ac_yr = frappe.db.get_value("Academic Year", {'custom_current_academic_year': 1}, 'name')
+    students = frappe.get_all("Program Enrollment", filters={'program': program, 'docstatus': 1, 'academic_year': ac_yr, 'custom_status': "Current student"}, fields=['student'])
+    for student in students:
+        try:
+            if not frappe.db.exists("CBSE Confirmation", {'student': student.student}):
+                doc = frappe.new_doc("CBSE Confirmation")
+                doc.student = student.student
+                doc.save(ignore_permissions=True)
+        except Exception as e:
+            frappe.log_error("CBSE", e)
---
+++ edu_quality/edu_quality/doctype/cbse_confirmation/cbse_confirmation_list.js
@@ -0,0 +1,46 @@
+frappe.listview_settings['CBSE Confirmation'] = {
+    refresh: function (listview) {
+        listview.page.add_menu_item(__("Generate Confirmations"), function () {
+
+            let d = new frappe.ui.Dialog({
+                title: "Generate Confirmations",
+                fields: [
+                    {
+                        fieldname: "program",
+                        label: "Class",
+                        fieldtype: "Link",
+                        options: "Program"
+                    }
+                ],
+                size: "small",
+                primary_action_label: "Generate",
+                primary_action(values) {
+                    frappe.call({
+                        method: "edu_quality.edu_quality.doctype.cbse_confirmation.cbse_confirmation.generate_confirmations",
+                        type: "POST",
+                        args: {
+                            program: values.program
+                        },
+                        callback: function (response) {
+                            if (response.message.status == "1") {
+                                frappe.show_alert({
+                                    message: __("Enqueued for generation"),
+                                    indicator: 'green'
+                                });
+                                d.hide();
+                            }
+                            else {
+                                frappe.show_alert({
+                                    message: __(response.message.error),
+                                    indicator: 'red'
+                                });
+                            }
+                        }
+                    })
+                }
+            })
+            d.show();
+        });
+    }
+
+}
