fca388a2a0b447b1bd7d01ce0847985f9d4c1d5a
aguyran <aguyran@gmail.com>
2026-03-11 07:27:45 +0000

    fix: optimize order_by in print format

--- edu_quality/public/py/utils.py
+++ edu_quality/public/py/utils.py
@@ -660,7 +660,7 @@ def get_div_students(division, ref_no=None):
 
 
 def get_descriptive_result(assessment_group, student):
-
+    assess_plan_qb = frappe.qb.DocType("Assessment Plan")
     assess_res_qb = frappe.qb.DocType("Assessment Result")
     assess_res_de_qb = frappe.qb.DocType("Assessment Result Detail")
     assess_ques_res_qb = frappe.qb.DocType("Descriptive Question")
@@ -677,7 +677,7 @@ def get_descriptive_result(assessment_group, student):
             .inner_join(assess_ques_res_qb)
             .on(assess_res_de_qb.custom_question == assess_ques_res_qb.name)
         )
-        .orderby(assess_res_de_qb.custom_order)
+        .orderby(assess_plan_qb.custom_order, assess_res_de_qb.custom_order)
         .select(
             assess_res_de_qb.name.as_("criteria_name"),
             assess_res_qb.name,
