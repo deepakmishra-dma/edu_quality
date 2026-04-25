acb7f623030c5c639cf3d13e06ac16a8c5424207
aguyran <aguyran@gmail.com>
2026-04-25 13:14:04 +0000

    fix/toppers-result

--- edu_quality/edu_quality/doctype/assessment_group_result/assessment_group_result.py
+++ edu_quality/edu_quality/doctype/assessment_group_result/assessment_group_result.py
@@ -15,22 +15,76 @@ class AssessmentGroupResult(Document):
     def onload(self):
         """Load Existing results for quick view"""
         self.load_results()
+        self.load_subj_results()
 
     def load_results(self):
         """Load `results` from the database"""
         self.results = []
         plans = get_all_plans(self)
         results = get_all_results(self, plans)
-
+        print(results, "hher")
         for result in results:
             self.append("results", result)
+
         return results
+
+    def load_subj_results(self):
+        self.subject_wise_result = []
+        plans = get_all_plans(self)
+        results = get_all_results(self, plans)
+        subject_wise_results = self.get_combined_subj_result(results)
+
+        for result in subject_wise_results:
+            self.append("subject_wise_result", result)
+        # print(subject_wise_results)
+        return subject_wise_results
+
+    def get_combined_subj_result(self, results):
+        subject_wise_group = {}
+        for result in results:
+            subject = result.get("course")
+            scoring_type = result.get("scoring_type")
+            if scoring_type.lower() != "marks":
+                continue
+            if subject not in subject_wise_group:
+                subject_wise_group[subject] = [result]
+            else:
+                subject_wise_group[subject].append(result)
+
+        subject_wise_results = []
+
+        for subject in subject_wise_group:
+            subject_wise_result = {
+                "subject": subject,
+                "maximum_score": 0,
+                "score": 0,
+                "percentage": 0,
+            }
+
+            for result in subject_wise_group[subject]:
+                score = result.get("total_score")
+                maximum_score = result.get("maximum_score")
+                subject_wise_result["maximum_score"] += maximum_score or 0
+                subject_wise_result["score"] += score or 0
+
+            if subject_wise_result["maximum_score"]:
+
+                subject_wise_result["percentage"] = (
+                    subject_wise_result["score"] / subject_wise_result["maximum_score"]
+                ) * 100
+
+            subject_wise_results.append(subject_wise_result)
+        return subject_wise_results
 
     def calculate_class_rank(self):
         assess_gr_qb = frappe.qb.DocType("Assessment Group Result")
         query = (
             frappe.qb.from_(assess_gr_qb)
-            .where((assess_gr_qb.docstatus.isin([0, 1])))
+            .where(
+                (assess_gr_qb.docstatus.isin([0, 1]))
+                & (assess_gr_qb.assessment_group == self.assessment_group)
+                & (assess_gr_qb.program == self.program)
+            )
             .select(
                 assess_gr_qb.combined_percentage,
                 assess_gr_qb.name,
@@ -69,6 +123,8 @@ class AssessmentGroupResult(Document):
             .where(
                 (assess_gr_qb.docstatus.isin([0, 1]))
                 & (assess_gr_qb.student.isin(student_list or [None]))
+                & (assess_gr_qb.assessment_group == self.assessment_group)
+                & (assess_gr_qb.program == self.program)
             )
             .select(
                 assess_gr_qb.combined_percentage,
@@ -111,10 +167,21 @@ class AssessmentGroupResult(Document):
         self.calculate_total_score()
         self.class_rank = self.calculate_class_rank() or 0
         self.division_rank = self.calculate_div_rank() or 0
-        self.results = []
 
-    def validate(self):
+        self.clean_virtual_child_tables()
+
+    def before_save(self):
+        self.clean_virtual_child_tables()
+
+    def before_cancel(self):
+        self.clean_virtual_child_tables()
+
+    def before_update_after_submit(self):
+        self.clean_virtual_child_tables()
+
+    def clean_virtual_child_tables(self):
         self.results = []
+        self.subject_wise_result = []
 
 
 def get_all_plans(assessment_group_res_doc):
@@ -123,6 +190,7 @@ def get_all_plans(assessment_group_res_doc):
         "Assessment Plan",
         filters={
             "assessment_group": assessment_group_res_doc.get("assessment_group"),
+            "docstatus": 1,
         },
     )
     plans = [plan.get("name") for plan in plans]
@@ -142,7 +210,7 @@ def get_all_results(assessment_group_res_doc, plans=[]):
             (ar_qb.assessment_group == assess_group)
             & (ar_qb.assessment_plan.isin(plans))
             & (ar_qb.student == student)
-            & (ar_qb.docstatus.isin([0, 1]))
+            & (ar_qb.docstatus.isin([1]))
         )
         .select(
             ar_qb.name.as_("assessment_result"),
@@ -150,6 +218,7 @@ def get_all_results(assessment_group_res_doc, plans=[]):
             ar_qb.custom_processed_grade.as_("total_grade"),
             ar_qb.custom_scoring_type.as_("scoring_type"),
             ar_qb.maximum_score,
+            ar_qb.course,
             ar_qb.custom_processed_percentage.as_("percentage"),
         )
     )
